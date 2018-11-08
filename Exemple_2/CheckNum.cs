a2b48253b3e65e6ca15423150d68eae969fe6f77
pqo1993 <pqoalla@gmail.com>
2018-11-09 00:16:44 +0400

    Class And Object

---
+++ Exemple_2/CheckNum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_2_Use_of_parameters
+{
+    class CheckNum
+    {
+        public bool IsPrim(int x)
+        {
+            if (x <= 1)
+                return false;
+            for (int i = 2; i <= x / i; i++)
+                if ((x % i) == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Exemple_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_2_Use_of_parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNum result = new CheckNum();
+            for (int i = 2; i < 10; i++)
+                if (result.IsPrim(i)) Console.WriteLine(i + " Prime number.");
+                else Console.WriteLine(i + "  not prime number.");
+            Console.ReadKey();
+        }
+    }
+}
