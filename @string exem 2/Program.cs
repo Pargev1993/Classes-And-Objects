6260224fb988a1d711c62bfec0fca935db1f230e
pqo1993 <pqoalla@gmail.com>
2018-11-16 20:50:39 +0400

    works

---
+++ @string exem 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _string_exem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " Madam I am Adam! ";
+
+            Console.WriteLine(s.Contains('M'));// stugum e m tari parunakutyun@ textum
+            Console.WriteLine(s.Length);//s-i erkarutyun@
+            Console.Write(s.PadLeft(29,'*'));//Avelacnum e texti qanak + avelacac tverin *
+            Console.WriteLine(new string('*', 13));
+            Console.WriteLine(s.Remove(10, 5));//jjnum en 10-rd toxic sksac 5 hat sinfol
+            Console.WriteLine(s.Replace(' ', '+'));//sinvoli poxarinum e +
+            Console.WriteLine(s.Replace("Madam","MADAM"));//poxarinum e hin sinvolner@ nor sivolnerov
+            Console.WriteLine(s.ToLower());//bolor tarer@ mecatar
+            Console.WriteLine(s.ToUpper());//bolor tarer@ poqratar
+            Console.WriteLine(s.Trim());//jnjum e skzbic ev verjic avel probelner@
+            Console.ReadKey();
+            //Static Strings
+            string ss = string.Concat(s, "!!!!!!", s, "999");//concotenaciaya anum bolor@ mek toxi vra
+            Console.WriteLine(ss);
+            ss = string.Copy(s);// s text@ copy e anum veragrelov ss-in
+            Console.WriteLine(ss);
+            ss = string.Empty;//avelacnum e datark taracq
+            Console.WriteLine(s[10]);
+            String l = string.Copy(s)+"!!" ;//copyin avelacnum e sinvolner
+            Console.WriteLine(l);
+            // Console.WriteLine(s==l);
+            Console.WriteLine(s.Equals(l));//stugum e s havasar e ardyoq l-in
+            Console.WriteLine(s.CompareTo(l));//hashvum e s-i ev l-i sinvolneri qanaki tarberutyun@
+            Console.WriteLine(Object.ReferenceEquals(s, l));//stugum e ardyoq Hipum S-@ ev l-@ nuynnen nuyn hxum@ unen te voch
+            ss = s;
+            Console.WriteLine(object.ReferenceEquals(s,ss));
+            int age = 25;
+            int work = 10;
+            string sss = string.Format("ypu work {0},{1}", age, work);
+            Console.WriteLine(sss);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ @string1 exem 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. New line \nNew outher line \"Line\"");
+            Console.WriteLine("2.New\touther\tline");
+            Console.WriteLine("3.New outher line line line \routher");
+            Console.WriteLine("4.New outher \aline ");
+            Console.WriteLine("5.New outher  \\n \nline ");
+            Console.WriteLine("6.File road is D:\\film\\...");
+            Console.WriteLine(@"7.File 
+
+road is D:\film\...\n \r \n \a");
+            Console.WriteLine("8. New outher line" + Environment.NewLine + "\"Line\"");
+            Console.WriteLine("9.The name machinery :" + Environment.MachineName);
+            Console.WriteLine("10.The processor count:" + Environment.ProcessorCount);
+            Console.WriteLine("11.The user name :" + Environment.UserName);
+            Console.WriteLine("12.The versia of mashine :" + Environment.Version);
+            Console.WriteLine("13.The versia of program :" + Environment.OSVersion);
+            Console.WriteLine("C format {0:C}", 5665.166);//Currency
+            Console.WriteLine("D9 format {0:D9}", 5665);//Decimal number
+            Console.WriteLine("E format {0:E}", 5665.166);//Exponential
+            Console.WriteLine("F3 format {0:F3}", 9999.9999);//Fixed comma
+            Console.WriteLine("G format {0:G}", 5665.166);//Are common
+            Console.WriteLine("N format {0:N}", 5665.166);//Numeric
+            Console.WriteLine("P format {0:P}", 5665.166);//Percent
+            Console.WriteLine("R format {0:R}", 5665.166);//Transmission
+            Console.WriteLine("X format {0:X}", 5665);//Hexadecimal
+            Console.ReadKey();
+        }
+    }
+}
