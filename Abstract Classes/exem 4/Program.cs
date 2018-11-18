26f26a316cd28b8461e502a4874ea3263dbe4948
pqo1993 <pqoalla@gmail.com>
2018-11-18 19:08:21 +0400

    Work Exemples

---
+++ Abstract Classes/exem 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exem_4
+{
+    abstract class AbstractClass
+    {
+        public AbstractClass()
+        {
+            Console.WriteLine("1 abstract class");
+            this.AbstractMethod();
+            Console.WriteLine("2 Abstract  Class");
+
+        }
+        public abstract void AbstractMethod();
+    }
+    class ConcretClass:AbstractClass
+    {
+        string s = "First";
+        public ConcretClass()
+        {
+            Console.WriteLine("3 concret Class");
+            s = "Second";
+        }
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("Thets oll {0}",s);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass Instance = new ConcretClass();
+            Instance.AbstractMethod();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interface exem 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_exem_1
+{
+    interface IInterface
+    {
+        void eat();
+    }
+    class Myckass:IInterface
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eat");
+        }
+    }
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myckass v;
+            v.eat();
+        }
+    }
+}
