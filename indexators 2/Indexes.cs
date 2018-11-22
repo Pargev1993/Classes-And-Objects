b0f06dc5256e745a97fd95475dc23ec9abb58e03
pqo1993 <pqoalla@gmail.com>
2018-11-23 01:11:21 +0400

    indexator with for

---
+++ indexators 2/Indexes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexators_2
+{
+    class Indexes
+    {
+        private string[] Array =new string[5];
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Array.Length)
+                    return Array[index];
+                else
+                    return "error";
+            }
+            set
+            {
+                if (index >= 0 && index < Array.Length)
+                    Array[index] = value;
+                else
+                    Console.WriteLine("Error");
+
+            }
+        }
+
+    }
+}
---
+++ indexators 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexators_2
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexes My = new Indexes();
+            // My[0] = "string 1";
+            // My[1] = "string 2";
+            // My[2] = "string 3";
+            // My[3] = "string 4";
+            // My[4] = "string 5";
+            for (int i = 0; i < 5; i++)
+            {
+                My[i] =string.Format( "string {0}",i);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(My[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
