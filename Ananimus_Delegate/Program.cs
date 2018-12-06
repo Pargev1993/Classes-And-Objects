1ceca48a0374f718bba3e5fb1bf2776be8c99840
pqo1993 <pqoalla@gmail.com>
2018-12-06 21:54:01 +0400

    Ananimus  Method (delegate)

---
+++ Ananimus_Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_exem
+{
+    class Program
+    {
+
+        public delegate int Mydelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            int summand = 1, summand1 = 2, sum = 0;
+            Mydelegate mydelegate = delegate (int a, int b) { return a + b; };
+            sum = mydelegate(summand, summand1);
+            Console.WriteLine(sum);
+        }
+
+    }
+}
