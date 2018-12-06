0343f63d961e4650ed43966e2067634ac058bfd3
pqo1993 <pqoalla@gmail.com>
2018-12-06 22:30:15 +0400

    Lyambda (Delegate)

---
+++ Ananimus_Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ananimus_Delegate
+{
+    class Program
+    {
+        public delegate int Mydelegate(int x);
+        static void Main(string[] args)
+        {
+            Mydelegate mydelegate = delegate (int x) { return x * 2; };
+            mydelegate = (x) => { return x * 2; };
+            mydelegate = x => { return x * 2; };
+            int res = mydelegate(4);
+            Console.WriteLine(res);
+
+        }
+    }
+}
