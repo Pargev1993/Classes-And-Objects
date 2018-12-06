392e17e94ef82aaffc1055900e73ca0177ec1b00
pqo1993 <pqoalla@gmail.com>
2018-12-06 23:29:41 +0400

    Delegates (Recursia)

---
+++ Delegats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegats
+{
+    public delegate void Mydelegate(int x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydelegate mydelegate = null;
+            mydelegate = (int i) =>
+              {
+                  i--;
+                  Console.WriteLine(i);
+                  if (i > 0)
+                      mydelegate(i);
+                  Console.WriteLine(i);
+
+              };
+            mydelegate(3);
+        }
+    }
+}
+
