53bbf8167baba5687da3d85a1349577ab30360be
pqo1993 <pqoalla@gmail.com>
2018-12-06 22:22:45 +0400

    Ananimu Nethod(Delegate  and ref/out)

---
+++ Ananimus_Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ananimus_Method
+{
+    class Program
+    {
+        public delegate void Mydelegate(ref int a, ref int b, out int c);
+        static void Main(string[] args)
+        {
+            int summand = 1, summand1 = 2, sum = 0;
+            Mydelegate mydelegate = delegate (ref int a,ref int b,out int c) {  a++; b++; c = a + b; };
+            mydelegate(ref summand, ref summand1, out sum);
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ Ananimus_Method/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ananimus_Method")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ananimus_Method")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("24c9307d-1d7d-48ca-ae80-0ad890e1872c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
